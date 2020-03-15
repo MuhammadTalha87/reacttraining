d3e232f87d87e40d056ae771c692d2af66993923
Muhammad <muhammad.talha@stud.hslu.ch>
2020-03-15 23:49:32 +0100

    Testing

--- src/FirstComponent.js
+++ src/FirstComponent.js
@@ -4,6 +4,7 @@ import ReactDom from 'react-dom'
 class FirstComponent extends Component{
 
     render(){
+        <p>This program is a just basics of react</p>
         return <h1>Hello Its my first Program</h1>
     }
 }
