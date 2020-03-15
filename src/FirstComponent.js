669e457c6ec61cf0de88e11c6db0f58f09a7b551
Muhammad <muhammad.talha@stud.hslu.ch>
2020-03-15 23:46:19 +0100

    project cloned

---
+++ src/FirstComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import ReactDom from 'react-dom'
+
+class FirstComponent extends Component{
+
+    render(){
+        return <h1>Hello Its my first Program</h1>
+    }
+}
+
+export default FirstComponent;
