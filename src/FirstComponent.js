f6535fc8d55dc3fde4b50fd856d7ccf479511df7
Muhammad <muhammad.talha@stud.hslu.ch>
2020-03-17 00:18:25 +0100

    Card is added and buttons from material ui is also added

--- src/FirstComponent.js
+++ src/FirstComponent.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
-import ReactDom from 'react-dom'
+import { Button } from '@material-ui/core';
+import { Card, Avatar } from 'antd';
+import  './App.css'
+//import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+
+const { Meta } = Card;
+// import ReactDom from 'react-dom'
 
 class FirstComponent extends Component{
 
+//     constructor(props) {
+//         super(props);
+//         this.clickme = this.clickme.bind(this);
+//       }
+
+clickme(){
+var x = 5;
+ var square = x*x;
+
+ alert("square of "+x+ "is"+square);
+
+   }
+
     render(){
-        <p>This program is a just basics of react</p>
-        return <h1>Hello Its my first Program</h1>
+        return (
+            <div >
+    <Card className="card"
+    
+    
+    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+  >
+      <Button  style={{margin: 15
+      }} variant="contained" color="primary" onClick={this.clickme}>Click me</Button>
+  </Card>
+  </div>
+        // <Button  variant="contained" color="primary" onClick={this.clickme}>Click me</Button>
+        )
     }
 }
 
